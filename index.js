572fc1047f59f8d609d8197b1569c1c47cf8ed1a
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-01-25 21:25:04 +0100

    Add project popup dynamic

--- index.js
+++ index.js
@@ -36,16 +36,68 @@ document.querySelectorAll('.menu_link').forEach((link) => {
 // Project popup
 
 // Create project object
-const projectPop = {
-  name: 'Project name goes here',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-  featuredImage: 'img/project1.svg',
-  technologiesOne: 'HTML/CSS',
-  technologiesTwo: 'Ruby on Rails',
-  technologiesThree: 'JavaScript',
-  linkToLiveVersion: 'https://ismailcourr.ml',
-  linkToSource: 'https://ismailcourr.ml',
-};
+const projectPop = [
+  {
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    featuredImage: 'img/project1.svg',
+    technologiesOne: 'HTML/CSS',
+    technologiesTwo: 'Ruby on Rails',
+    technologiesThree: 'JavaScript',
+    linkToLiveVersion: 'https://ismailcourr.ml',
+    linkToSource: 'https://ismailcourr.ml',
+  },
+  {
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    featuredImage: 'img/project2.svg',
+    technologiesOne: 'HTML/CSS',
+    technologiesTwo: 'Ruby on Rails',
+    technologiesThree: 'JavaScript',
+    linkToLiveVersion: 'https://ismailcourr.ml',
+    linkToSource: 'https://ismailcourr.ml',
+  },
+  {
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    featuredImage: 'img/project3.svg',
+    technologiesOne: 'HTML/CSS',
+    technologiesTwo: 'Ruby on Rails',
+    technologiesThree: 'JavaScript',
+    linkToLiveVersion: 'https://ismailcourr.ml',
+    linkToSource: 'https://ismailcourr.ml',
+  },
+  {
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    featuredImage: 'img/project4.svg',
+    technologiesOne: 'HTML/CSS',
+    technologiesTwo: 'Ruby on Rails',
+    technologiesThree: 'JavaScript',
+    linkToLiveVersion: 'https://ismailcourr.ml',
+    linkToSource: 'https://ismailcourr.ml',
+  },
+  {
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    featuredImage: 'img/project5.svg',
+    technologiesOne: 'HTML/CSS',
+    technologiesTwo: 'Ruby on Rails',
+    technologiesThree: 'JavaScript',
+    linkToLiveVersion: 'https://ismailcourr.ml',
+    linkToSource: 'https://ismailcourr.ml',
+  },
+  {
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    featuredImage: 'img/project6.svg',
+    technologiesOne: 'HTML/CSS',
+    technologiesTwo: 'Ruby on Rails',
+    technologiesThree: 'JavaScript',
+    linkToLiveVersion: 'https://ismailcourr.ml',
+    linkToSource: 'https://ismailcourr.ml',
+  },
+];
 
 // create html elements
 const projectPopup = document.createElement('div');
@@ -67,68 +119,70 @@ const buttonContainer = document.createElement('div');
 const liveDemo = document.createElement('button');
 const sourceCode = document.createElement('button');
 
-// create content
-closeBtn.classList.add('fas', 'fa-times', 'popup_close');
-
-projectName.textContent = projectPop.name;
-projectName.classList.add('popup_name');
-
-listItemOne.textContent = projectPop.technologiesOne;
-listItemOne.classList.add('popup_language');
-listItemTwo.textContent = projectPop.technologiesTwo;
-listItemTwo.classList.add('popup_language');
-listItemThree.textContent = projectPop.technologiesThree;
-listItemThree.classList.add('popup_language');
-list.classList.add('popup_languages');
-list.append(listItemOne, listItemTwo, listItemThree);
-
-image.setAttribute('src', projectPop.featuredImage);
-image.classList.add('popup_img');
-slider.appendChild(image);
-
-slider.classList.add('slider');
-sliderCountainer.appendChild(slider);
-
-sliderCountainer.classList.add('slider_container');
-
-miniImage.setAttribute('src', projectPop.featuredImage);
-miniImage.classList.add('popup_mini_img');
-const miniImageTwo = miniImage.cloneNode(true);
-const miniImageThree = miniImageTwo.cloneNode(true);
-const miniImageFoor = miniImageThree.cloneNode(true);
-miniImageCountainer.append(miniImage, miniImageTwo, miniImageThree, miniImageFoor);
-miniImageCountainer.classList.add('mini_img');
-
-imageContainer.append(sliderCountainer, miniImageCountainer);
-imageContainer.classList.add('img_slider');
-
-description.classList.add('popup_text');
-description.textContent = projectPop.description;
-
-liveDemo.classList.add('popup_btn');
-liveDemo.innerHTML = `<a href = "${projectPop.linkToLiveVersion}">See live <i class="fa fa-arrow-right"></i></a>`;
-
-sourceCode.classList.add('popup_btn');
-sourceCode.innerHTML = `<a href = "${projectPop.linkToSource}">See source <i class="fab fa-github"></i></a>`;
-
-buttonContainer.classList.add('popup_buttons');
-buttonContainer.append(liveDemo, sourceCode);
-
-projectPopup.classList.add('project_pop');
-projectPopup.append(closeBtn, projectName, list, imageContainer, description, buttonContainer);
-
-document.body.appendChild(projectPopup);
-
 // Listen for a click on the projects links or close button
 closeBtn.addEventListener('click', () => {
   projectPopup.style.display = 'none';
   closeBtn.style.display = 'none';
+  projectPopup.innerHTML = '';
+  miniImageCountainer.innerHTML = '';
 });
 
 const projectLinks = document.querySelectorAll('.project_btn');
-projectLinks.forEach((btn) => {
+projectLinks.forEach((btn, index) => {
   btn.addEventListener('click', () => {
     projectPopup.style.display = 'flex';
     closeBtn.style.display = 'block';
+
+    // create content
+    closeBtn.classList.add('fas', 'fa-times', 'popup_close');
+
+    projectName.textContent = projectPop[index].name;
+    projectName.classList.add('popup_name');
+
+    listItemOne.textContent = projectPop[index].technologiesOne;
+    listItemOne.classList.add('popup_language');
+    listItemTwo.textContent = projectPop[index].technologiesTwo;
+    listItemTwo.classList.add('popup_language');
+    listItemThree.textContent = projectPop[index].technologiesThree;
+    listItemThree.classList.add('popup_language');
+    list.classList.add('popup_languages');
+    list.append(listItemOne, listItemTwo, listItemThree);
+
+    image.setAttribute('src', projectPop[index].featuredImage);
+    image.classList.add('popup_img');
+    slider.appendChild(image);
+
+    slider.classList.add('slider');
+    sliderCountainer.appendChild(slider);
+
+    sliderCountainer.classList.add('slider_container');
+
+    miniImage.setAttribute('src', projectPop[index].featuredImage);
+    miniImage.classList.add('popup_mini_img');
+    const miniImageTwo = miniImage.cloneNode(true);
+    const miniImageThree = miniImageTwo.cloneNode(true);
+    const miniImageFoor = miniImageThree.cloneNode(true);
+    miniImageCountainer.append(miniImage, miniImageTwo, miniImageThree, miniImageFoor);
+    miniImageCountainer.classList.add('mini_img');
+
+    imageContainer.append(sliderCountainer, miniImageCountainer);
+    imageContainer.classList.add('img_slider');
+
+    description.classList.add('popup_text');
+    description.textContent = projectPop[index].description;
+
+    liveDemo.classList.add('popup_btn');
+    liveDemo.innerHTML = `<a href = "${projectPop[index].linkToLiveVersion}">See live <i class="fas fa-share-square"></i></a>`;
+
+    sourceCode.classList.add('popup_btn');
+    sourceCode.innerHTML = `<a href = "${projectPop[index].linkToSource}">See source <i class="fab fa-github"></i></a>`;
+
+    buttonContainer.classList.add('popup_buttons');
+    buttonContainer.append(liveDemo, sourceCode);
+
+    projectPopup.classList.add('project_pop');
+    projectPopup.append(closeBtn, projectName, list, imageContainer, description, buttonContainer);
+
+    document.body.appendChild(projectPopup);
   });
 });
